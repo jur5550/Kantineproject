f0934df419edcf3114e122a1c1fe9d33daac915b
Jur van Zonneveld <jurvanzonneveld@gmail.com>
2015-01-13 15:59:12 +0100

    Bezig met teweiniggeldexception

---
+++ TeWeinigGeldException.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class TeWeinigGeldException here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TeWeinigGeldException
+{
+     TeWeinigGeldException();
+     
+     TeWeinigGeldException(Exception e);
+     
+     TeWeinigGeldException(String message);
+
+}
+
